ac51db2b1c6ec1b86db3b451c0d1098ecc291c72
DucThang <ducthang0271555@gmail.com>
2025-11-27 11:49:54 +0700

    refactor: add column image_url to model.hash_tag

--- server/app/models/hash_tag.py
+++ server/app/models/hash_tag.py
@@ -5,6 +5,7 @@ class HashTag(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
+    image_url = db.Column(db.String(255), nullable=True)
 
     def __repr__(self):
         return f'<HashTag {self.name}>'
